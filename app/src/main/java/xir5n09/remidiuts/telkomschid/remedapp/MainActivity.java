58664c4100efc4e554cbd759d8113d2ca9a9fecd
Danang P. Kalingga <juslemon@outlook.com>
2017-06-07 12:28:16 +0700

    Button Functioning

--- app/src/main/java/xir5n09/remidiuts/telkomschid/remedapp/MainActivity.java
+++ app/src/main/java/xir5n09/remidiuts/telkomschid/remedapp/MainActivity.java
@@ -1,7 +1,10 @@
 package xir5n09.remidiuts.telkomschid.remedapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,37 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button bt1 = (Button) findViewById(R.id.button2);
+        bt1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, SwipeActivity.class));
+            }
+        });
+
+        Button bt2 = (Button) findViewById(R.id.button3);
+        bt2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, TabActivity.class));
+            }
+        });
+
+        Button bt3 = (Button) findViewById(R.id.button4);
+        bt3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, SpinnerActivity.class));
+            }
+        });
+
+        Button bt4 = (Button) findViewById(R.id.button5);
+        bt4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, DrawerActivity.class));
+            }
+        });
     }
 }
